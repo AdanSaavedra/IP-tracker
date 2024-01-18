561598ce741a3e9e54671a37f8da06f080664cd1
Adan Saavedra <adansaavedraa@icloud.com>
2024-01-17 23:10:41 -0500

    add map an geolocation with ip

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-var map = L.map("map").setView([9.063523, -79.451966], 18);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-var marker = L.marker([9.063523, -79.451966]).addTo(map);
-var circle = L.circle([9.063523, -79.451966], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(map);
-var popup = L.popup();
+fetch(
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_a9gKZSoHOFDUzEKRrO2KCtN9cH5oE"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    initMap(data.location.lat, data.location.lng);
+  })
+  .catch((error) => console.error("Error with the location: ", error));
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
+function initMap(latitude, longitude) {
+  // Crea un mapa con Leaflet
+  const map = L.map("map").setView([latitude, longitude], 20);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
+    foo: "bar",
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Añade un marcador en la ubicación del usuario
+  var myIcon = L.icon({
+    iconUrl: "./images/icon-location.svg",
+    // iconSize: [38, 95],
+    // iconAnchor: [22, 94],
+    // popupAnchor: [-3, -76],
+    // shadowAnchor: [22, 94],
+  });
+  L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
 }
-
-map.on("click", onMapClick);
