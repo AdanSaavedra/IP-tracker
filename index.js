251d2ada10e9e67d3e28da00e1f61772a43ecbb3
Adan Saavedra <adansaavedraa@icloud.com>
2024-01-25 23:35:22 -0500

    finish desktop functionalities

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const ip = document.querySelector(".ipText");
+const ipp = document.querySelector(".ipText");
 const locationn = document.querySelector(".locationText");
 const timezone = document.querySelector(".timezoneText");
 const isp = document.querySelector(".ispText");
 let ipForm = document.getElementById("ipForm");
+var map;
+var tileLayer = L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}",
+  {
+    foo: "bar",
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
 fetch(
   "https://api.ipgeolocation.io/ipgeo?apiKey=208f5ca499974e30a1231096d09001fc"
 )
   .then((response) => response.json())
   .then((data) => {
-    initMap(data.latitude, data.longitude)
-    ip.innerHTML += `${data.ip}`;
-    locationn.innerHTML += `${data.city}, ${data.country_capital}`;
-    timezone.innerHTML += `${data.time_zone.offset}`;
-    isp.innerHTML += `${data.isp}`;
+    initMap(data.latitude, data.longitude);
+    ipp.innerHTML = `${data.ip}`;
+    locationn.innerHTML = `${data.city}, ${data.country_capital}`;
+    timezone.innerHTML = `${data.time_zone.offset}`;
+    isp.innerHTML = `${data.isp}`;
   })
   .catch((error) => console.error("Error with the location: ", error));
 
 function initMap(latitude, longitude) {
-  // Crea un mapa con Leaflet
-  var map = L.map("map").setView([latitude, longitude], 17);
+  if (map) {
+    map.setView([latitude, longitude], 17);
+  } else {
+    map = L.map("map").setView([latitude, longitude], 17);
+  }
+  tileLayer.addTo(map);
 
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
-    foo: "bar",
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  // Añade un marcador en la ubicación del usuario
   var myIcon = L.icon({
     iconUrl: "./images/icon-location.svg",
     // iconSize: [38, 95],
@@ -47,17 +53,18 @@ ipForm.addEventListener("submit", (e) => {
 });
 
 const searchIp = (ip) => {
-  ip.innerHTML += "";
+  map.removeLayer(tileLayer);
   fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_a9gKZSoHOFDUzEKRrO2KCtN9cH5oE&ipAddress=${ip}`
+    `https://api.ipgeolocation.io/ipgeo?apiKey=208f5ca499974e30a1231096d09001fc&ip=${ip}`
   )
     .then((response) => response.json())
     .then((data) => {
-      // initMap(data.location.lat, data.location.lng);
-      ip.innerHTML += `${data.ip}`;
-      locationn.innerHTML += `${data.location.city}, ${data.location.country}`;
-      timezone.innerHTML += `${data.location.timezone}`;
-      isp.innerHTML += `${data.isp}`;
+      console.log(data);
+      initMap(data.latitude, data.longitude);
+      ipp.innerHTML = `${data.ip}`;
+      locationn.innerHTML = `${data.city}, ${data.country_capital}`;
+      timezone.innerHTML = `${data.time_zone.offset}`;
+      isp.innerHTML = `${data.isp}`;
     })
     .catch((error) => console.error("Error with the location: ", error));
 };
